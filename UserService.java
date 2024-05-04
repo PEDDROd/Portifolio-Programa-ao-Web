e5eb94ca61f12ad6386d38454f5df8a03aa8b350
PEDDROd <147842921+PEDDROd@users.noreply.github.com>
2024-05-03 22:47:19 -0300

    Add files via upload

---
+++ UserService.java
@@ -0,0 +1,65 @@
+package com.example.demo.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.entities.User;
+import com.example.demo.repositories.UseRepository;
+import com.example.demo.services.exceptions.ResourceNotFoundException;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private  UseRepository repository;
+	
+	public List<User> findALL(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		
+		Optional<User> usuario = repository.findById(id);
+        return usuario.orElseThrow(() -> new ResourceNotFoundException(id) );
+	}
+	
+	
+	public User insert(User usuario) {
+		return repository.save(usuario);
+	}
+	
+	public void delete(Long id) {
+		
+		try {
+			
+			repository.deleteById(id);
+		} catch (ResourceNotFoundException e) {
+			
+			
+		}
+		
+		repository.deleteById(id);
+		
+	}
+	
+    public User update (Long id,User usuario) {
+    	
+    	try {
+    	User cadastro = repository.getReferenceById(id);
+    	updateDados(cadastro, usuario);
+    	return repository.save(cadastro);
+    	} catch (RuntimeException e) {
+    		throw new ResourceNotFoundException(id);
+    	}
+    }
+
+	private void updateDados(User cadastro, User usuario) {
+		cadastro.setNome(usuario.getNome());
+		cadastro.setEmail(usuario.getEmail());
+		cadastro.setTelefone(usuario.getTelefone());
+		
+	}
+}
